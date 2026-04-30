3856eba711d5851d8d899723fa251aadeb6f4de8
Mauricio Dall Onder <londer11@icloud.com>
2026-04-30 14:58:44 +0200

    Atualizar o AppShell.tsx

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { useState } from "react";
+
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
+import Toolbar from "@mui/material/Toolbar";
+
 import { Sidebar } from "./SideBar";
 import { Topbar } from "./TopBar";
 
@@ -13,22 +16,59 @@ type AppShellProps = {
 };
 
 export function AppShell({ children }: AppShellProps) {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  function handleDrawerToggle() {
+    setMobileOpen((current) => !current);
+  }
+
+  function handleDrawerClose() {
+    setMobileOpen(false);
+  }
+
   return (
-    <Box sx={{ display: "flex", minHeight: "100vh" }}>
-      <Topbar drawerWidth={drawerWidth} />
-      <Sidebar drawerWidth={drawerWidth} />
+    <Box
+      sx={{
+        display: "flex",
+        minHeight: "100vh",
+        bgcolor: "background.default",
+        overflowX: "hidden",
+      }}
+    >
+      <Topbar
+        drawerWidth={drawerWidth}
+        onMenuClick={handleDrawerToggle}
+      />
+
+      <Sidebar
+        drawerWidth={drawerWidth}
+        mobileOpen={mobileOpen}
+        onMobileClose={handleDrawerClose}
+      />
 
       <Box
         component="main"
         sx={{
           flexGrow: 1,
-          minHeight: "100vh",
-          bgcolor: "background.default",
+          minWidth: 0,
+          width: {
+            xs: "100%",
+            md: `calc(100% - ${drawerWidth}px)`,
+          },
+          overflowX: "hidden",
         }}
       >
         <Toolbar />
 
-        <Container maxWidth="xl" sx={{ py: 4 }}>
+        <Container
+          maxWidth="xl"
+          sx={{
+            py: { xs: 2, sm: 3, md: 4 },
+            px: { xs: 2, sm: 3 },
+            maxWidth: "100%",
+            overflowX: "hidden",
+          }}
+        >
           {children}
         </Container>
       </Box>
